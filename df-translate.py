4435fa4eb7739fa62898bc6c6fda8d7858d2e672
insolor <insolor@localhost>
2016-09-01 09:10:41 +0300

    Add password entry field

--- df-translate.py
+++ df-translate.py
@@ -8,8 +8,12 @@ from transifex.api import TransifexAPI
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
+        
         self.entry_username = ttk.Entry(self)
         self.entry_username.pack()
+        
+        self.entry_password = ttk.Entry(self, show='\u2022')  # 'bullet' symbol
+        self.entry_password.pack()
 
 
 app = App()
