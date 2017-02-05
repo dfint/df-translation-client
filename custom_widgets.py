084c4250041c79f8d72a4954cf95bc15d473a182
Mike Manturov <insolor@users.noreply.github.com>
2017-02-05 21:01:22 +0300

    Add ComboboxCustom and use it everywhere :)

--- custom_widgets.py
+++ custom_widgets.py
@@ -31,3 +31,28 @@ class EntryCustom(ttk.Entry):
     @text.setter
     def text(self, value: str):
         self._set(value)
+
+
+class ComboboxCustom(ttk.Combobox):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _set(self, value):
+        self.delete(0, tk.END)
+        self.insert(0, value)
+
+    @property
+    def text(self):
+        return self.get()
+
+    @text.setter
+    def text(self, value: str):
+        self._set(value)
+
+    @property
+    def values(self):
+        return self['values']
+
+    @values.setter
+    def values(self, values: tuple):
+        self['values'] = values
