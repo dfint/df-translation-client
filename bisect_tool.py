442bb1a17ba047a1c8e6ec6e480f616000b73109
Mike Manturov <insolor@users.noreply.github.com>
2018-09-27 22:25:40 +0300

    Partially implement bisect tool

---
+++ bisect_tool.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+
+class Bisect(tk.Frame):
+    def __init__(self, *args, strings=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._strings = strings
+        self.tree = tree = ttk.Treeview(self)
+        tree.grid()
+        tree["columns"] = ("start", "end", "start_string", "end_string")
+        tree["displaycolumns"] = tree["columns"][-2:]
+
+        if strings:
+            self.insert_node(start=0, end=len(strings)-1)
+        
+        ttk.Button(self, text="Split", command=self.split_selected_node).grid()
+
+    def insert_node(self, parent_node='', index='end', start=0, end=0):
+        if start != end:
+            text='[{} : {}] ({} strings)'.format(start, end, end-start+1)
+            values = (start, end, repr(self._strings[start]), repr(self._strings[end]))
+        else:
+            text='[{} : {}] ({} string)'.format(start, end, end-start+1)
+            values = (start, end, repr(self._strings[start]))
+
+        self.tree.insert(parent_node, index, text=text, open=True, values=values)
+
+    def split_selected_node(self):
+        tree = self.tree
+        item = tree.selection()
+        if item:
+            start, end = map(int, tree.item(item[0], option="values")[:2])
+            if start != end:
+                mid = (start + end) // 2
+                self.insert_node(item, start=start, end=mid)
+                self.insert_node(item, start=mid+1, end=end)
+
+    @property
+    def filtered_dictionary(self):
+        return self._strings
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    Bisect(root, strings='Lorem ipsum dolor sit amet'.split()).pack()
+    root.mainloop()
