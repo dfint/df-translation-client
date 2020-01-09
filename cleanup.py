06f56856792e24ca8d921871872e49aaf73f8f00
insolor <insolor@gmail.com>
2020-01-09 08:41:03 +0300

    Remove U+2006 characters from the translation

--- cleanup.py
+++ cleanup.py
@@ -15,4 +15,4 @@ def cleanup_spaces(d: iter, exclusions=None):
 
 def cleanup_special_symbols(s):
     # TODO: Make this mapping customizable
-    return s.translate({0xfeff: None, 0x2019: "'", 0x201d: '"', 0x2014: '-'})
+    return s.translate({0xfeff: None, 0x2019: "'", 0x201d: '"', 0x2014: '-', 0x200b: None})
