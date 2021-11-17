c49152f80bec0d5a324908091ff4bc60a4ede926
insolor <insolor@gmail.com>
2021-11-17 10:08:20 +0300

    Rewrite frame_translate_external_files.py using Grid helper, refactoring

--- frames/frame_translate_external_files.py
+++ frames/frame_translate_external_files.py
@@ -11,6 +11,7 @@ from dfrus.patch_charmap import get_codepages
 from natsort import natsorted
 
 from config import Config
+from tkinter_helpers import Grid
 from widgets import FileEntry, ScrollbarFrame
 from widgets.custom_widgets import Combobox, Listbox
 from .frame_patch import filter_codepages
@@ -21,9 +22,9 @@ class TranslateExternalFiles(tk.Frame):
     def get_languages(directory):
         languages = set()
         directory = Path(directory)
-        for filename in directory.glob('*.po'):
-            with open(directory / filename, encoding='utf-8') as file:
-                languages.add(parse_po.PoReader(file).meta['Language'])
+        for filename in directory.glob("*.po"):
+            with open(directory / filename, encoding="utf-8") as file:
+                languages.add(parse_po.PoReader(file).meta["Language"])
 
         return sorted(languages)
 
@@ -50,8 +51,8 @@ class TranslateExternalFiles(tk.Frame):
     @staticmethod
     def filter_files_by_language(directory: Path, language):
         for filename in directory.glob("*.po"):
-            with open(filename, encoding='utf-8') as file:
-                if parse_po.PoReader(file).meta['Language'] == language:
+            with open(filename, encoding="utf-8") as file:
+                if parse_po.PoReader(file).meta["Language"] == language:
                     yield filename.name
 
     def update_listbox_translation_files(self, language=None):
@@ -68,33 +69,33 @@ class TranslateExternalFiles(tk.Frame):
             files = self.filter_files_by_language(directory, language)
             codepages = get_codepages().keys()
             for file in files:
-                with open(directory / file, 'r', encoding='utf-8') as fn:
+                with open(directory / file, "r", encoding="utf-8") as fn:
                     pofile = parse_po.PoReader(fn)
-                    strings = [cleanup_string(entry['msgstr']) for entry in pofile]
+                    strings = [cleanup_string(entry["msgstr"]) for entry in pofile]
                 codepages = filter_codepages(codepages, strings)
             self.combo_encoding.values = natsorted(codepages)
 
             if self.combo_encoding.values:
                 self.combo_encoding.current(0)
             else:
-                self.combo_encoding.text = 'cp437'
+                self.combo_encoding.text = "cp437"
 
     def bt_search(self, translate=False):
         patterns = {
-            'raw/objects': dict(
-                po_filename='raw-objects',
+            "raw/objects": dict(
+                po_filename="raw-objects",
                 func=translate_raws,
             ),
-            'data': dict(
-                po_filename='uncompressed',
+            "data": dict(
+                po_filename="uncompressed",
                 func=lambda *args: translate_compressed(*args),
             ),
-            'data/speech': dict(
-                po_filename='speech',
+            "data/speech": dict(
+                po_filename="speech",
                 func=lambda *args: translate_plain_text(*args, join_paragraphs=False),
             ),
-            'raw/objects/text': dict(
-                po_filename='text',
+            "raw/objects/text": dict(
+                po_filename="text",
                 func=lambda *args: translate_plain_text(*args, join_paragraphs=False),
             ),
         }
@@ -106,9 +107,9 @@ class TranslateExternalFiles(tk.Frame):
         for cur_dir in Path(base_path).rglob("*"):
             if cur_dir.is_dir():
                 for pattern in patterns:
-                    if cur_dir.match('*/' + pattern):
+                    if cur_dir.match("*/" + pattern):
                         self.listbox_found_directories.append(f"Matched {pattern!r} pattern")
-                        base_name = patterns[pattern]['po_filename']
+                        base_name = patterns[pattern]["po_filename"]
                         postfix = self.combo_language.text
                         po_filename = f"{base_name}_{postfix}.po"
 
@@ -125,7 +126,7 @@ class TranslateExternalFiles(tk.Frame):
                             return
 
                         if translate:
-                            func = patterns[pattern]['func']
+                            func = patterns[pattern]["func"]
                             for filename in func(po_file_path, cur_dir, self.combo_encoding.get()):
                                 # print(filename, file=sys.stderr)
                                 self.listbox_found_directories.append(filename)
@@ -135,68 +136,70 @@ class TranslateExternalFiles(tk.Frame):
 
     def __init__(self, master, config: Config, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
-        self.config_section = config.init_section(section_name='translate_external_files')
+        self.config_section = config.init_section(section_name="translate_external_files")
         config_section = self.config_section
 
-        tk.Label(self, text='Dwarf Fortress root path:').grid()
+        with Grid(self, sticky=tk.NSEW, padx=2, pady=2) as grid:
+            self.file_entry_df_root_path = FileEntry(
+                dialog_type="askdirectory",
+                default_path=config_section.get("df_root_path", ''),
+                on_change=lambda text: self.save_path_to_config("df_root_path", text),
+            )
 
-        self.file_entry_df_root_path = FileEntry(
-            self,
-            dialog_type='askdirectory',
-            default_path=config_section.get('df_root_path', ''),
-            on_change=lambda text: self.save_path_to_config('df_root_path', text),
-        )
-        self.file_entry_df_root_path.grid(row=0, column=1, sticky='WE')
+            grid.add_row("Dwarf Fortress root path:", self.file_entry_df_root_path)
 
-        tk.Label(self, text="Translation files' directory:").grid()
+            self.file_entry_translation_files = FileEntry(
+                dialog_type="askdirectory",
+                default_path=config_section.get("translation_files_path", ''),
+                on_change=lambda path: self.save_path_to_config("translation_files_path", path),
+            )
 
-        self.file_entry_translation_files = FileEntry(
-            self,
-            dialog_type='askdirectory',
-            default_path=config_section.get('translation_files_path', ''),
-            on_change=lambda path: self.save_path_to_config('translation_files_path', path),
-        )
-        self.file_entry_translation_files.grid(row=1, column=1, sticky='WE')
+            grid.add_row("Translation files' directory:", self.file_entry_translation_files)
 
-        tk.Label(self, text="Language:").grid()
-        self.combo_language = Combobox(self)
-        self.combo_language.grid(row=2, column=1, sticky='WE')
+            self.combo_language = Combobox()
+            self.combo_language.grid(row=2, column=1, sticky="WE")
 
-        directory = Path(self.file_entry_translation_files.text)
-        if directory.exists():
-            languages = self.get_languages(directory)
-            self.combo_language.values = languages
-            if languages:
-                self.combo_language.current(0)
+            grid.add_row("Language:", self.combo_language)
 
-        def on_combo_language_change(_event):
-            self.update_listbox_translation_files()
+            directory = Path(self.file_entry_translation_files.text)
+            if directory.exists():
+                languages = self.get_languages(directory)
+                self.combo_language.values = languages
+                if languages:
+                    self.combo_language.current(0)
+
+            def on_combo_language_change(_event):
+                self.update_listbox_translation_files()
+                self.update_combo_encoding()
+
+            self.combo_language.bind("<<ComboboxSelected>>", on_combo_language_change)
+
+            self.combo_encoding = Combobox()
+            grid.add_row("Encoding:", self.combo_encoding)
+
             self.update_combo_encoding()
 
-        self.combo_language.bind('<<ComboboxSelected>>', on_combo_language_change)
+            scrollbar_frame = ScrollbarFrame(widget_factory=Listbox, show_scrollbars=tk.VERTICAL)
+            grid.add_row(scrollbar_frame, ...)
 
-        tk.Label(self, text="Encoding:").grid()
-        self.combo_encoding = Combobox(self)
-        self.combo_encoding.grid(row=3, column=1, sticky=tk.EW)
+            self.listbox_translation_files: Listbox = scrollbar_frame.widget
+            self.update_listbox_translation_files(language=self.combo_language.text)
 
-        self.update_combo_encoding()
+            with Grid(tk.Frame(), sticky=tk.EW, padx=1) as buttons:
+                buttons.add_row(ttk.Button(text="Search", command=self.bt_search),
+                                ttk.Button(text="Translate", command=lambda: self.bt_search(translate=True)))
 
-        scrollbar_frame = ScrollbarFrame(self, Listbox, show_scrollbars=tk.VERTICAL)
-        scrollbar_frame.grid(columnspan=2, sticky=tk.NSEW)
+                buttons.columnconfigure(0, weight=1)
+                buttons.columnconfigure(1, weight=1)
 
-        self.listbox_translation_files: Listbox = scrollbar_frame.widget
-        self.update_listbox_translation_files(language=self.combo_language.text)
+                grid.add_row(buttons.parent, ...)
 
-        ttk.Button(self, text='Search', command=self.bt_search).grid()
-        ttk.Button(self, text='Translate', command=lambda: self.bt_search(translate=True)).grid(row=5, column=1)
+            scrollbar_frame = ScrollbarFrame(widget_factory=Listbox, show_scrollbars=tk.VERTICAL)
+            grid.add_row(scrollbar_frame, ...).configure(weight=1)
 
-        scrollbar_frame = ScrollbarFrame(self, Listbox, show_scrollbars=tk.VERTICAL)
-        scrollbar_frame.grid(columnspan=2, sticky=tk.NSEW)
-        self.grid_rowconfigure(scrollbar_frame.grid_info()['row'], weight=1)
+            self.listbox_found_directories: Listbox = scrollbar_frame.widget
 
-        self.listbox_found_directories: Listbox = scrollbar_frame.widget
-
-        self.grid_columnconfigure(1, weight=1)
+            grid.columnconfigure(1, weight=1)
 
     def update(self):
         super().update()
