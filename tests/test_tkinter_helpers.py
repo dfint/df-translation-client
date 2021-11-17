6b8bd25bc1579bd64080b3bf60ad30e63be6bf68
insolor <insolor@gmail.com>
2021-11-17 17:00:31 +0300

    Test that an old value is restored if an exception is raised

--- tests/test_tkinter_helpers.py
+++ tests/test_tkinter_helpers.py
@@ -1,3 +1,5 @@
+import pytest
+
 from tkinter_helpers import Packer, set_parent
 
 
@@ -8,8 +10,10 @@ def test_set_parent(mocker):
     old_default_root = mocker.Mock(name="old default_root")
     default_root_wrapper.default_root = old_default_root
 
-    with set_parent(mocker.Mock(name="parent")) as parent:
-        assert default_root_wrapper.default_root == parent
+    with pytest.raises(ValueError):
+        with set_parent(mocker.Mock(name="parent")) as parent:
+            assert default_root_wrapper.default_root == parent
+            raise ValueError
 
     assert default_root_wrapper.default_root == old_default_root
 
@@ -23,9 +27,11 @@ def test_packer(mocker):
 
     widget = mocker.Mock(name="widget")
     options = dict(side="left", expand=1, fill="X", padx=1)
-    with Packer(mocker.Mock(name="parent"), **options) as packer:
-        assert default_root_wrapper.default_root == packer.parent
-        packer.pack_all(widget)
-        widget.pack.assert_called_with(**options)
+    with pytest.raises(ValueError):
+        with Packer(mocker.Mock(name="parent"), **options) as packer:
+            assert default_root_wrapper.default_root == packer.parent
+            packer.pack_all(widget)
+            widget.pack.assert_called_with(**options)
+            raise ValueError
 
     assert default_root_wrapper.default_root == old_default_root
