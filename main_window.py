5a8a5fd0098d051c81d3610ec97ad3ef61cf824b
insolor <insolor@gmail.com>
2021-11-18 14:21:07 +0300

    Refactoring of the main window class

--- main_window.py
+++ main_window.py
@@ -11,25 +11,30 @@ from tkinter_helpers import pack_expand, set_parent
 
 class MainWindow(tk.Tk):
     def save_settings_repeatedly(self, config, delay=500):
-        if hasattr(self, 'notebook'):
+        if hasattr(self, "notebook"):
             nb = self.notebook
             if nb.tabs():
-                self.config_section['last_tab_opened'] = nb.tabs().index(nb.select())
+                self.config_section["last_tab_opened"] = nb.tabs().index(nb.select())
 
         self.after(delay, self.save_settings_repeatedly, config, delay)
         config.save_settings()
 
-    def init_notebook(self, config: Config):
-        with set_parent(ttk.Notebook(self)) as notebook:
-            notebook.add(DownloadTranslationsFrame(config=config), text='Download translations')
-            notebook.add(PatchExecutableFrame(config=config, debug=self.app.debug), text='Patch executable file')
-            notebook.add(TranslateExternalFiles(config=config), text='Translate external text files')
+    def update_selected_tab(self):
+        selected_tab_name = self.notebook.select()
+        selected_tab = self.notebook.nametowidget(selected_tab_name)
+        selected_tab.update()
 
-            tab = self.config_section.get('last_tab_opened', 0)
+    def init_notebook(self, config: Config) -> ttk.Notebook:
+        with set_parent(ttk.Notebook(self)) as notebook:
+            notebook.add(DownloadTranslationsFrame(config=config), text="Download translations")
+            notebook.add(PatchExecutableFrame(config=config, debug=self.app.debug), text="Patch executable file")
+            notebook.add(TranslateExternalFiles(config=config), text="Translate external text files")
+
+            tab = self.config_section.get("last_tab_opened", 0)
             if 0 <= tab < len(notebook.tabs()):
                 notebook.select(tab)
 
-            notebook.bind('<<NotebookTabChanged>>', lambda _event: notebook.nametowidget(notebook.select()).update())
+            notebook.bind("<<NotebookTabChanged>>", lambda _event: self.update_selected_tab())
 
             return notebook
 
@@ -41,13 +46,16 @@ class MainWindow(tk.Tk):
         super().report_callback_exception(exc_type, exc_value, exc_traceback)
 
         filename, line, *_ = traceback.extract_tb(exc_traceback).pop()
-        message = '{}: {}\n{}, Line: {}\n'.format(exc_type.__name__, exc_value, filename, line)
-        messagebox.showerror('Unhandled Exception', message)
+        messagebox.showerror(
+            "Unhandled Exception",
+            f"{exc_type.__name__}: {exc_value}\n"
+            f"{filename}, Line: {line}"
+        )
 
     def __init__(self, app):
         super().__init__()
         self.app = app
         config = self.app.config
-        self.config_section = config.init_section('application', dict(last_tab_opened=0))
+        self.config_section = config.init_section("application", dict(last_tab_opened=0))
         self.notebook = self.init_notebook(config)
         pack_expand(self.notebook)
