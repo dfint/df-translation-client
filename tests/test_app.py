e1361d1320635146a64e80128c25a34c281fc78e
insolor <insolor@gmail.com>
2022-06-18 11:51:00 +0300

    Fix test_app test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,17 @@
+import asyncio
+
 import pytest
 
 from df_translation_client.app import App
 
 
-@pytest.mark.timeout(1000)
+@pytest.mark.timeout(1)
 def test_app():
     app = App(ignore_config_file=True, debug=True)
-    app.main_window.after(100, app.main_window.quit)
+
+    async def close_after_100ms():
+        await asyncio.sleep(0.1)
+        app.main_window.quit()
+
+    asyncio.get_event_loop().create_task(close_after_100ms())
     app.run()
