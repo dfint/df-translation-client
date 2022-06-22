c6594877af5abf69f3e7abd021a2438c49e61152
insolor <insolor@gmail.com>
2022-06-22 09:45:38 +0300

    Add a basic test for HighlightedSpacesItem

---
+++ tests/test_dialog_do_not_fix_spaces.py
@@ -0,0 +1,10 @@
+import pytest
+
+from df_translation_client.frames.dialog_do_not_fix_spaces import HighlightedSpacesItem
+
+
+@pytest.mark.parametrize("text, expected", [
+    ["  test   ", "••test•••"]
+])
+def test_highlight_spaces(text, expected):
+    assert str(HighlightedSpacesItem(text)) == expected
