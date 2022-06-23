e3e1a42dd5b743972f690f0ed601613bdb1283a4
insolor <insolor@gmail.com>
2022-06-23 15:45:02 +0300

    Raise NotImplementedError in AbstractDownloader's methods to exclude them from coverage

--- df_translation_client/downloaders/abstract_downloader.py
+++ df_translation_client/downloaders/abstract_downloader.py
@@ -7,17 +7,17 @@ from df_translation_client.downloaders.common import DownloadStage
 class AbstractDownloader(ABC):
     @abstractmethod
     async def connect(self) -> None:
-        ...
+        raise NotImplementedError
 
     @abstractmethod
     async def async_downloader(self, language: str, resources: List[str], file_path_pattern: str) \
             -> AsyncIterable[DownloadStage]:
-        ...
+        raise NotImplementedError
 
     @abstractmethod
     async def list_resources(self) -> List[str]:
-        ...
+        raise NotImplementedError
 
     @abstractmethod
     async def list_languages(self, resource_slug: str) -> List[str]:
-        ...
+        raise NotImplementedError
