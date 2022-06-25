117d0a4cbe51e4d2a80918995e100103f5b594bb
insolor <insolor@gmail.com>
2022-06-25 11:28:15 +0300

    Use fliter() on the text() strategy instead of strip() in the test
    (thanks to @ArjanCodes for the recent video about hypothesis)

--- tests/test_dialog_do_not_fix_spaces.py
+++ tests/test_dialog_do_not_fix_spaces.py
@@ -1,15 +1,18 @@
 import string
 
-from df_translation_client.frames.dialog_do_not_fix_spaces import HighlightedSpacesItem, SPACE_PLACEHOLDER
 from hypothesis import given, strategies as st
+
+from df_translation_client.frames.dialog_do_not_fix_spaces import HighlightedSpacesItem, SPACE_PLACEHOLDER
 
 
 @given(
     st.text(alphabet=" "),
-    st.text(alphabet=string.digits + string.ascii_letters + string.punctuation + " "),
+    st.text(alphabet=string.digits + string.ascii_letters + string.punctuation + " ").filter(
+        lambda s: not (s.startswith(" ") or s.endswith(" "))
+    ),
     st.text(alphabet=" "),
 )
 def test_highlight_spaces(leading_spaces, text, trailing_spaces):
-    text_with_spaces = leading_spaces + text.strip() + trailing_spaces
+    text_with_spaces = leading_spaces + text + trailing_spaces
     expected_result = SPACE_PLACEHOLDER * len(leading_spaces) + text.strip() + SPACE_PLACEHOLDER * len(trailing_spaces)
     assert str(HighlightedSpacesItem(text_with_spaces)) == expected_result
