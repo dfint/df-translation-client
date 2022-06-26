df66bc8de8b18a6dc22664e01d4f358d397b3416
insolor <insolor@gmail.com>
2022-06-26 13:24:13 +0300

    Migrate from aiohttp to httpx

--- df_translation_client/downloaders/github.py
+++ df_translation_client/downloaders/github.py
@@ -1,10 +1,8 @@
-import io
-import json
 import traceback
 from pathlib import PurePosixPath
 from typing import AsyncIterable, List, Mapping
 
-import aiohttp
+import httpx
 
 from df_translation_client.downloaders.abstract_downloader import AbstractDownloader
 from df_translation_client.downloaders.common import DownloadStage, StatusEnum
@@ -16,28 +14,24 @@ class GithubDownloader(AbstractDownloader):
 
     BASE_URL = PurePosixPath("raw.githubusercontent.com/dfint/translations-backup/main/")
 
-    def __init__(self):
-        pass
-
     async def connect(self) -> None:
         url = "https://" + str(GithubDownloader.BASE_URL / "metadata.json")
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                self.metadata = json.loads(await response.text())
-                self.resources = list(self.metadata)
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+            self.metadata = response.json()
+            self.resources = list(self.metadata)
 
     async def async_downloader(self, language: str, resources: List[str], file_path_pattern: str) \
             -> AsyncIterable[DownloadStage]:
-        async with aiohttp.ClientSession() as session:
+        async with httpx.AsyncClient() as client:
             for resource in resources:
                 yield DownloadStage(resource, StatusEnum.DOWNLOADING, None)
                 url = "https://" + str(GithubDownloader.BASE_URL / "translations" / self.metadata[resource][language])
                 try:
-                    async with session.get(url) as response:
-                        file_name = file_path_pattern.format(resource=resource, language=language)
-                        with open(file_name, "wb") as file:
-                            async for chunk in response.content.iter_chunked(io.DEFAULT_BUFFER_SIZE):
-                                file.write(chunk)
+                    response = await client.get(url)
+                    file_name = file_path_pattern.format(resource=resource, language=language)
+                    with open(file_name, "wb") as file:
+                        file.write(response.content)
                 except Exception:
                     yield DownloadStage(resource, StatusEnum.FAILED, traceback.format_exc())
                 else:
